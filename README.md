e0dc8483836cd5674bf0597a599ff376cff6b8e4
Ketz7 <ketanmaurya@gmail.com>
2022-01-06 17:22:37 +0530

    all tests completed

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # [URL Shortener Microservice](https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice)
+
+## Language and Framework
+![Javascript](https://img.shields.io/badge/Language-Javascript-brightgreen) ![Expres](https://img.shields.io/badge/Framework-Express-brightgreen)
+
+___
+## User Stories
+[x] You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+
+[x] When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+
+[x] If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+
+___
+
+This is a Node.js (with Express.js) little application which is part of the FCC Back End Certification. It shortens any given url which is in a valid format. Given the short url it will redirect you to the main site.
+
+Example 
+```
+https://www.google.com -> {"original_url":"https://www.google.com","short_url":18}
+
+going to /app/shorturl/18 -> redirects to [https"](https://www.google.com)
+```
--- server.js
+++ server.js
@@ -1,24 +1,95 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+
+let bodyParser = require("body-parser");
+const { request } = require("express");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
+});
+
+/* Database Connection */
+
+let uri = "<your uri from mongo>";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let urlSchema = new mongoose.Schema({
+  original: { type: String, required: true },
+  short: Number,
+});
+
+let Url = mongoose.model("Url", urlSchema);
+
+let responseObject = {};
+
+app.post(
+  "/api/shorturl",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    let inputUrl = req.body["url"];
+
+    let urlRegex = new RegExp(
+      /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+    );
+
+    if (!inputUrl.match(urlRegex)) {
+      res.json({ error: "invalid url" });
+      return;
+    }
+
+    responseObject["original_url"] = inputUrl;
+
+    let inputShort = 1;
+
+    Url.findOne({})
+      .sort({ short: "desc" })
+      .exec((error, result) => {
+        if (!error && result != undefined) {
+          inputShort = result.short + 1;
+        }
+        if (!error)
+          Url.findOneAndUpdate(
+            { original: inputUrl },
+            { original: inputUrl, short: inputShort },
+            { new: true, upsert: true },
+            (error, savedUrl) => {
+              if (!error) {
+                responseObject["short_url"] = savedUrl.short;
+                res.json(responseObject);
+              }
+            }
+          );
+      });
+  }
+);
+
+app.get("/api/shorturl/:input", (req, res) => {
+  let input = req.params.input;
+
+  Url.findOne({ short: input }, (err, result) => {
+    if (!err && result != undefined) {
+      res.redirect(result.original);
+    } else {
+      res.json("URL not found");
+    }
+  });
 });
